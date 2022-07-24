8f4354857fe4ddd915c5d4be22218a08cfe21ced
Heet <heet@loadshare.net>
2022-07-24 22:00:03 +0530

    Created

---
+++ MavenExample/src/main/java/com/caveofprogramming/example/App.java
@@ -0,0 +1,18 @@
+package com.caveofprogramming.example;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Multiset<String> animals = HashMultiset.create();
+        animals.add("cat");
+        animals.add("cat");
+        animals.add("dog");
+
+        animals.forEach(System.out::println);
+    }
+}
